4dd295f552027e0692561634bea5d335aaba78dc
sudarsanraji <sudarsan99.raji@gmail.com>
2022-11-01 19:23:53 +0530

    bolwing class created and updated

---
+++ BowlingGame.java
@@ -0,0 +1,17 @@
+
+/**
+ * @author Sudarsan
+ *
+ */
+package com.bnpp.bowl;
+
+public class BowlingGame {
+	
+	  int getScore(String input) {
+		    FramesBuilder framesBuilder = new FramesBuilder();
+		    BowlingScoreCal bowlingCalculator = new BowlingScoreCal();
+		    return bowlingCalculator.calculate(framesBuilder.build(input));
+		  }
+
+
+}
